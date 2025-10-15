e8165ee3bc864bb00cbddc2fdd9220d0b1a54266
JmmonJeremy <suc19002@byui.edu>
2025-10-15 12:54:06 -0700

    Did 1st section of assignment and changed project name to track phases.

---
+++ src/app/contacts/contact-item/contact-item.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-item',
+  standalone: false,
+  templateUrl: './contact-item.html',
+  styleUrl: './contact-item.css'
+})
+export class ContactItem {
+ @Input() contact: Contact
+}
